6743a8f50c43481f4d0187ad119ccaeef1467ec6
creavin <thomas.creavin@gmail.com>
2020-04-16 21:10:10 +0100

    added vimrc dot files

--- fetchDotfiles.sh
+++ fetchDotfiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cp -r ~/.config/xfce4 .
+cp -r ~/.vim .
+
 cp ~/.Xmodmap .
+cp ~/.vimrc .
--- overwriteDotfiles.sh
+++ overwriteDotfiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cp -r xfce4 ~/.config
+cp -r .vim ~/
+
 cp .Xmodmap ~/.Xmodmap
+cp .vimrc ~/.vimrc
