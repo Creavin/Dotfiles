2d86b006a72b232967cfe6ec7163ec39c580348b
creavin <thomas.creavin@gmail.com>
2020-05-31 16:07:16 +0100

    Add fonts to dot files

--- fetchDotfiles.sh
+++ fetchDotfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp -r ~/.config/xfce4 .
 cp -r ~/.vim/UltiSnips .
+cp -r ~/.local/share/fonts .
 
 cp ~/.Xmodmap .
 cp ~/.vimrc .
+
--- overwriteDotfiles.sh
+++ overwriteDotfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp -r xfce4 ~/.config
 cp -r .vim/UltiSnips ~/.vim
+cp -r fonts ~/.local/share
 
 cp .Xmodmap ~/.Xmodmap
 cp .vimrc ~/.vimrc
+
