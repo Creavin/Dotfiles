7ec25c3bb762840b888320aae7fc59d3afb784bf
creavin <thomas.creavin@gmail.com>
2022-09-17 10:42:27 +0100

    Add aws autocomplete setup script

---
+++ bash/setup_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add auto-complete for the aws cli
+aws_auto_complete=/bin/aws_completer
+
+if [ -f "$aws_auto_complete" ]
+then
+    echo "Adding auto-complete for the AWS cli"
+    complete -C '/usr/local/bin/aws_completer' aws
+else
+    echo "$aws_auto_complete does not exist. NOT adding auto-complete for the AWS cli"
+fi
+
